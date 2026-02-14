700847cfca6beb1c06b7c6c3635dc562b6607b8c
rojarida <romjaydav@gmail.com>
2026-02-14 14:37:32 -0500

    included token usage

--- main.py
+++ main.py
@@ -12,13 +12,24 @@ def main():
             "Missing GEMINI_API_KEY. Insert into .env file. (Reminder: Do not share API keys)"
         )
 
+    contents="Using only a single paragraph, what is the difference between information technology and information security?"
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=contents
     )
 
-    print(response.text)
+    usage = response.usage_metadata
+    if usage is None:
+        raise RuntimeError(
+            "No usage_metadata returned."
+        )
+    
+    print(f"User prompt: {contents}")
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
